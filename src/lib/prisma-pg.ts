3a8961dfbb911321974426c61020863d55b9b9a8
lucasbasep <lucas.base29@gmail.com>
2024-11-05 23:38:55 -0300

    advasdvas

--- src/lib/prisma-pg.ts
+++ src/lib/prisma-pg.ts
@@ -6,7 +6,7 @@ import ws from "ws";
 neonConfig.webSocketConstructor = ws;
 
 export function PrismaGetInstance() {
-    const pool = new Pool({ connectionString: process.env.POSTGRES_URL});
+    const pool = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL});
     const adapter = new PrismaNeon(pool);
     const prisma = new PrismaClient({ adapter });
 
